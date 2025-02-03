79a98f5b907f22083ae8202bd693e8f62c055201
farnoosh afshinrad <farnoosh.afshinrad@mediaengine.it>
2025-02-03 19:12:07 +0100

    delete some files

--- apps/backend/Midi-Extractor/example.py
+++
@@ -1,8 +0,0 @@
-import os
-from SongFeaturesRetriever import SongFeaturesRetriever
-
-if __name__ == "__main__":
-    features = SongFeaturesRetriever()
-
-    path = os.path.join( os.path.dirname(os.path.abspath(__file__)), 'tmp', 'ABBA.m4a')
-    features.getVocalTranscription(path, splitInstrumental=True)
